b513334a4e414961498ddab0feb05ac5f24c05e0
Treyton Davis <37282419+tdavis422@users.noreply.github.com>
2021-03-01 09:57:14 -0600

    Added local files

--- App.js
+++ App.js
@@ -1,21 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
+import {
+	StyleSheet,
+	View,
+	Alert
+} from 'react-native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Status from './components/Status';
+import MessageList from './components/MessageList';
+
+import {
+	createImageMessage,
+	createLocationMessage,
+	createTextMessage,
+} from './utils/MessageUtils';
+
+export default class App extends React.Component {
+	state = {
+		messages: [
+			createImageMessage('https://unsplash.it/300/300'),
+			createTextMessage('World'),
+			createTextMessage('Hello'),
+			createLocationMessage({
+				latitude: 37.78825,
+				longitude: -122.4324,
+			}),
+		],
+	};
+	
+	handlePressMessage = ({ id, type }) => {
+		switch(type) {
+			case 'text':
+				Alert.alert(
+					'Delete message?',
+					'Are you sure you want to permanently delete this message?',
+					[
+						{
+							text: 'Cancel',
+							style: 'cancel',
+						},
+						{
+							text: 'Delete',
+							style: 'destructive',
+							onPress: () => {
+								const { messages } = this.state;
+								this.setState({
+									messages: messages.filter(
+										message => message.id !== id,
+									),
+								});
+							},
+						},
+					],
+				);
+			break;
+			
+			default:
+				break;
+		}
+	};
+	
+	renderMessageList() {
+		const { messages } = this.state;
+		
+		return (
+			<View style={styles.content}>
+				<MessageList
+					messages={messages}
+					onPressMessage={this.handlePressMessage}
+				/>
+			</View>
+		);
+	};
+	
+	renderInputMethodEditor() {
+		return (
+			<View style={styles.inputMethodEditor}></View>
+		);
+	};
+	
+	renderToolbar() {
+		return (
+			<View style={styles.toolbar}></View>
+		);
+	};
+	
+	render() {
+		return(
+			<View style={styles.container}>
+				<Status />
+				{this.renderMessageList()}
+				{this.renderToolbar()}
+				{this.renderInputMethodEditor()}
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+	},
+	
+	content: {
+		flex: 1,
+		backgroundColor: 'white',
+	},
+	
+	inputMethodEditor: {
+		flex: 1,
+		backgroundColor: 'white',
+	},
+	
+	toolbar: {
+		borderTopWidth: 1,
+		borderTopColor: 'rgba(0,0,0,0.04)',
+		backgroundColor: 'white',
+	},
 });
